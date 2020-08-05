9db2225a2cee543bdee00adf1436a3ea71f51063
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-08-05 12:59:32 +0530

    Update read_file.tcl

--- read_file.tcl
+++ read_file.tcl
@@ -1,7 +1,7 @@
 #grep -v "*" analog.sp
 
 #grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_' | grep 'M\|D\|R' analog.sp > analog.sp1
-grep 'M\|D\|R' *.sp | grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_\|cload\|pulse'> temp 
+grep 'M\|D\|R' *.sp | grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_\|cload\|pulse\|Rl'> temp 
 #sed -i '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_/d' | grep 'M\|D\|R' analog.sp
 #grep M analog.sp> analog.sp1 
 awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' temp > spice_final.sp
