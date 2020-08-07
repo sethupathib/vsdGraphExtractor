75b230e27db33be65e4a6798d2b59f406655199e
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-08-07 10:43:42 +0530

    Update and rename read_file.tcl to read_spice.sh

--- read_spice.sh
+++ read_spice.sh
@@ -5,3 +5,4 @@ grep 'M\|D\|R' *.sp | grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plo
 #sed -i '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_/d' | grep 'M\|D\|R' analog.sp
 #grep M analog.sp> analog.sp1 
 awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' temp > spice_final.sp
+sed -i 's/vdd/1/g' spice_final.sp
