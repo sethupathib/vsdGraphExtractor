7d873855ba826e11e8dbfbbe40605a43d3945f83
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-08-07 10:44:23 +0530

    Update read_spice.sh

--- read_spice.sh
+++ read_spice.sh
@@ -4,5 +4,5 @@
 grep 'M\|D\|R' *.sp | grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_\|cload\|pulse\|Rl'> temp 
 #sed -i '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_/d' | grep 'M\|D\|R' analog.sp
 #grep M analog.sp> analog.sp1 
-awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' temp > spice_final.sp
+awk '{print $2,$4}' temp > spice_final.sp
 sed -i 's/vdd/1/g' spice_final.sp
