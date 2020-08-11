01a6a5365a68094203d35d39e48cb56cd10f31ac
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-08-11 16:13:11 +0530

    Delete read_spice.sh

--- read_spice.sh
+++
@@ -1,8 +0,0 @@
-#grep -v "*" analog.sp
-
-#grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_' | grep 'M\|D\|R' analog.sp > analog.sp1
-grep 'M\|D\|R' *.sp | grep -v '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_\|cload\|pulse\|Rl'> temp 
-#sed -i '.include\|.end\|.dc\|.control\|.endc\|.model\|plot\|*\|run\|Vd_/d' | grep 'M\|D\|R' analog.sp
-#grep M analog.sp> analog.sp1 
-awk '{print $2,$4}' temp > spice_final.sp
-sed -i 's/vdd/1/g' spice_final.sp
